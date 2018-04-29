1567b41254d19e524d1750f64054992d6fb03358
yinjunjian0 <2926685103@qq.com>
2018-04-29 18:36:05 +0800

    修改sql

--- elesystem.sql
+++ elesystem.sql
@@ -2,15 +2,15 @@
 Navicat MySQL Data Transfer
 
 Source Server         : localhost_3306
-Source Server Version : 50717
+Source Server Version : 50617
 Source Host           : localhost:3306
 Source Database       : elesystem
 
 Target Server Type    : MYSQL
-Target Server Version : 50717
+Target Server Version : 50617
 File Encoding         : 65001
 
-Date: 2018-01-16 10:14:32
+Date: 2018-04-29 18:35:52
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -29,7 +29,7 @@ CREATE TABLE `admin_login` (
 -- ----------------------------
 -- Records of admin_login
 -- ----------------------------
-INSERT INTO `admin_login` VALUES ('1', '123', '123');
+INSERT INTO `admin_login` VALUES ('1', 'admin', 'admin');
 
 -- ----------------------------
 -- Table structure for dataadmin_login
@@ -62,9 +62,7 @@ CREATE TABLE `ele_gonggao` (
 -- ----------------------------
 -- Records of ele_gonggao
 -- ----------------------------
-INSERT INTO `ele_gonggao` VALUES ('1', '10月1日停水', '具体通知', '2018-01-02 16:52:37');
-INSERT INTO `ele_gonggao` VALUES ('2', '1月1日放假', '1月1日放假啊', '2017-12-05 16:52:45');
-INSERT INTO `ele_gonggao` VALUES ('3', 'das', 'sad', '2018-01-15 10:09:59');
+INSERT INTO `ele_gonggao` VALUES ('1', '1月5日停电通知', '由于电缆出线故障，故届时15-17时停电抢修。', '2018-01-02 16:52:37');
 
 -- ----------------------------
 -- Table structure for ele_gzbx
@@ -80,15 +78,15 @@ CREATE TABLE `ele_gzbx` (
   `progress` varchar(255) DEFAULT NULL,
   `updatetime` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of ele_gzbx
 -- ----------------------------
-INSERT INTO `ele_gzbx` VALUES ('1', 'yjj', 'home', '故障', '2017-12-12', '水龙头坏了', '处理中', '2017-12-28 17:48:02');
-INSERT INTO `ele_gzbx` VALUES ('3', 'yjj', '1', '故障', '2017-01-01', '水龙头坏了', '已查看', '2018-01-15 10:09:46');
-INSERT INTO `ele_gzbx` VALUES ('4', 'yjj', '1', '故障', '2017-11-11', '花洒', '已完成', '2018-01-11 15:55:03');
-INSERT INTO `ele_gzbx` VALUES ('5', 'lth', '1', '其他', '2017-11-01', '厕所', '已完成', '2017-12-28 17:48:03');
+INSERT INTO `ele_gzbx` VALUES ('1', 'yjj', '丁香g201', '故障', '2017-12-12', '水龙头坏了', '处理中', '2017-12-28 17:48:02');
+INSERT INTO `ele_gzbx` VALUES ('3', 'yjj', '丁香g201', '故障', '2017-01-01', '水龙头坏了', '已查看', '2018-01-15 10:09:46');
+INSERT INTO `ele_gzbx` VALUES ('4', 'yjj', '丁香g201', '故障', '2017-11-11', '花洒', '已查看', '2018-04-29 00:09:33');
+INSERT INTO `ele_gzbx` VALUES ('5', 'lth', '南风g501', '其他', '2017-11-01', '厕所', '已派人', '2018-04-29 00:09:33');
 
 -- ----------------------------
 -- Table structure for ele_ly
@@ -107,13 +105,7 @@ CREATE TABLE `ele_ly` (
 -- ----------------------------
 -- Records of ele_ly
 -- ----------------------------
-INSERT INTO `ele_ly` VALUES ('2', '123', '123', '1124124', '213412', '2017-12-28 17:39:09');
-INSERT INTO `ele_ly` VALUES ('5', '123', '123', '1124124', '213412', '2017-12-28 17:39:09');
-INSERT INTO `ele_ly` VALUES ('6', '123', '123', '1124124', '213412', '2017-12-28 17:39:09');
-INSERT INTO `ele_ly` VALUES ('9', '123', '123', '1124124', '213412', '2017-12-28 17:39:09');
-INSERT INTO `ele_ly` VALUES ('13', '123', '123', '1124124', '213412', '2017-12-28 17:39:09');
-INSERT INTO `ele_ly` VALUES ('14', '123', '123', '1124124', '213412', '2017-12-28 17:39:09');
-INSERT INTO `ele_ly` VALUES ('15', '123', '123', '1124124', '213412', '2017-12-28 17:39:09');
+INSERT INTO `ele_ly` VALUES ('2', '建议', 'yjj', '建议', '建议内容丰富一些', '2017-12-28 17:39:09');
 
 -- ----------------------------
 -- Table structure for user_gongyezd
@@ -126,14 +118,15 @@ CREATE TABLE `user_gongyezd` (
   `yongdian` varchar(255) DEFAULT NULL,
   `dianfei` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of user_gongyezd
 -- ----------------------------
-INSERT INTO `user_gongyezd` VALUES ('1', '123', '2018-02', '0', '282.6');
-INSERT INTO `user_gongyezd` VALUES ('2', '214', '2018-04', '02141', '1926.9');
-INSERT INTO `user_gongyezd` VALUES ('3', '124', '2018-02', '142', '127.8');
+INSERT INTO `user_gongyezd` VALUES ('4', 'gy', '2018-02', '123123', '95592.25');
+INSERT INTO `user_gongyezd` VALUES ('6', 'gy', '2018-05', '222', '199.8');
+INSERT INTO `user_gongyezd` VALUES ('7', 'gy', '2018-04', '123', '1');
+INSERT INTO `user_gongyezd` VALUES ('8', 'gy', '2018-01', '123', '110.7');
 
 -- ----------------------------
 -- Table structure for user_juminzd
@@ -149,16 +142,14 @@ CREATE TABLE `user_juminzd` (
   `yongdian` varchar(255) DEFAULT NULL,
   `dianfei` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=48 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=69 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of user_juminzd
 -- ----------------------------
 INSERT INTO `user_juminzd` VALUES ('43', 'yjj', '2018-02', '033', '033', '033', '0', '69.30');
-INSERT INTO `user_juminzd` VALUES ('44', 'yjj', '2018-05', '0124', '0124', '0124', '372', '260.40');
-INSERT INTO `user_juminzd` VALUES ('45', 'yjj', '2018-01', '0113', '031', '013', '157', '101.70');
-INSERT INTO `user_juminzd` VALUES ('46', 'lth', '2018-01', '124', '24', '21', '169', '108.30');
-INSERT INTO `user_juminzd` VALUES ('47', 'lth', '2018-02', '124', '24', '21', '169', '108.30');
+INSERT INTO `user_juminzd` VALUES ('58', 'yjj', '2018-05', '1', '0', '0', '1', '0.60');
+INSERT INTO `user_juminzd` VALUES ('68', 'yjj', '2018-04', '1', '0', '0', '1', '0.60');
 
 -- ----------------------------
 -- Table structure for user_login
@@ -174,14 +165,17 @@ CREATE TABLE `user_login` (
   `user_date` varchar(20) DEFAULT NULL,
   `yue` float(255,2) DEFAULT NULL,
   PRIMARY KEY (`id`,`user_name`)
-) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of user_login
 -- ----------------------------
-INSERT INTO `user_login` VALUES ('18', '124124', '124214', '12412', '4124', '工业用户', '2017-12-09', null);
-INSERT INTO `user_login` VALUES ('19', 'lth', '123', '13727521738', '2926685103@qq.com', '居民用户', '2017-12-07', '566.80');
-INSERT INTO `user_login` VALUES ('24', 'yjj', '123123', '13727521102', '2926685103@qq.com', '居民用电', '2018-01-02 ', '10.00');
+INSERT INTO `user_login` VALUES ('18', 'gy', '123', '12412', '4124', '工业用户', '2017-12-09', '120.00');
+INSERT INTO `user_login` VALUES ('19', 'lth', '123', '13727521738', '2926685103@qq.com', '居民用户', '2017-12-07', '458.50');
+INSERT INTO `user_login` VALUES ('24', 'yjj', '111111', '13727521102', '2926685103@qq.com', '居民用电', '2018-01-02 ', '1001.00');
+INSERT INTO `user_login` VALUES ('26', '1231231', '1231231', '12312312311', '123', '工业用电', '2018-04-29 12:05:45', null);
+INSERT INTO `user_login` VALUES ('27', 'yjj1996', 'a123456', '13727521102', '1@qq.com', '居民用电', '2018-04-29 13:17:36', '500.00');
+INSERT INTO `user_login` VALUES ('28', 'lth1995', '1234567', '12312312311', '1@qq.com', '居民用电', '2018-04-29 13:44:35', '0.00');
 
 -- ----------------------------
 -- Table structure for user_record
@@ -193,12 +187,11 @@ CREATE TABLE `user_record` (
   `date` varchar(10) DEFAULT NULL,
   `dianfei` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of user_record
 -- ----------------------------
 INSERT INTO `user_record` VALUES ('1', 'yjj', '2017-7', '222');
 INSERT INTO `user_record` VALUES ('2', 'lth', '2018-01', '108.30');
-INSERT INTO `user_record` VALUES ('3', 'lth', '2018-01', '108.30');
-INSERT INTO `user_record` VALUES ('4', 'lth', '2018-01', '108.30');
+INSERT INTO `user_record` VALUES ('5', 'lth', '2018-04', '108.30');
